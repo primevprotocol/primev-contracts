92e321b54526ecf305b19e46dc53345aa65b2819
Serhii Varakuta <flood.code@gmail.com>
2023-05-31 17:20:45 +0300

    Implement subscriptions and timelocks

--- contracts/BuilderStaking.sol
+++ contracts/BuilderStaking.sol
@@ -6,18 +6,39 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 uint256 constant BASE_DIVISOR = 1 ether;
 
 contract BuilderStaking is Ownable {
-    // builder -> minimal stake
-    mapping(address => uint256) public minimalStakes;
+    struct BuilderInfo {
+        uint256 minimalStake;
+        uint256 minimalSubscriptionPeriod;
+    }
 
-    // commitment -> amount
-    mapping(bytes32 => uint256) public stakes;
+    struct StakeInfo {
+        uint256 subscriptionEnd;
+        uint256 stake;
+    }
 
-    // address -> amount
-    mapping(address => uint256) public balances;
+    struct TimeLock {
+        uint256 initialAmount;
+        uint256 remainingAmount;
+        uint256 startTime;
+        uint256 lockDuration;
+    }
 
-    event StakeUpdated(address builder, bytes32 commitment, uint256 stake);
-    event BalanceUpdated(address builder, uint256 balance);
-    event MinimalStakeUpdated(address builder, uint256 minimalStake);
+    mapping(address => BuilderInfo) public builders;
+    mapping(bytes32 => StakeInfo) public stakes;
+    mapping(address => TimeLock[]) public timeLocks;
+
+    event StakeUpdated(
+        address builder,
+        bytes32 commitment,
+        uint256 stake,
+        uint256 subscriptionEnd
+    );
+    event BuilderUpdated(
+        address builder,
+        uint256 minimalStake,
+        uint256 minimalSubsriptionPeriod
+    );
+    event Withdrawal(address builder, uint256 amount);
 
     /**
      * @notice Deposit stake to builder on behalf of searcher using commitment hash
@@ -25,39 +46,139 @@ contract BuilderStaking is Ownable {
      * @param _commitment The commitment hash
      */
     function deposit(address _builder, bytes32 _commitment) public payable {
-        stakes[_commitment] += msg.value;
+        BuilderInfo memory info = builders[_builder];
+        StakeInfo memory stake = stakes[_commitment];
+        require(info.minimalStake > 0, "Builder minimal stake is not set");
+        require(
+            info.minimalSubscriptionPeriod > 0,
+            "Builder minimal subscription period is not set"
+        );
+        require(
+            msg.value >= info.minimalStake,
+            "Deposit amount is less than minimal stake"
+        );
+
+        stake.stake += msg.value;
+        uint256 subscriptionPeriod = _getSubscriptionPeriod(
+            info.minimalStake,
+            info.minimalSubscriptionPeriod,
+            msg.value
+        );
+
+        if (stake.subscriptionEnd > block.number) {
+            stake.subscriptionEnd += subscriptionPeriod;
+        } else {
+            stake.subscriptionEnd = block.number + subscriptionPeriod;
+        }
+
+        stakes[_commitment] = stake;
 
         address owner = owner();
-        uint256 builderAmount = (((msg.value * BASE_DIVISOR) / 100) * 80) / BASE_DIVISOR;
-        balances[_builder] += builderAmount;
-        balances[owner] += msg.value - builderAmount;
+        uint256 builderAmount = (((msg.value * BASE_DIVISOR) / 100) * 80) /
+            BASE_DIVISOR;
 
-        emit StakeUpdated(_builder, _commitment, stakes[_commitment]);
-        emit BalanceUpdated(_builder, balances[_builder]);
-        emit BalanceUpdated(owner, balances[owner]);
-    }
+        _lockFunds(_builder, builderAmount, info.minimalSubscriptionPeriod);
+        (bool sent, ) = owner.call{value: msg.value - builderAmount}("");
+        require(sent, "Failed to distribute funds");
 
-    /**
-     * @notice Withdraw balance from the contract
-     */
-    function withdraw() public {
-        uint256 balance = balances[msg.sender];
-        require(balance > 0, "Nothing to withdraw");
-
-        balances[msg.sender] = 0;
-        emit BalanceUpdated(msg.sender, 0);
-
-        (bool sent, ) = address(msg.sender).call{value: balance}("");
-        require(sent, "Failed to withdraw");
+        emit StakeUpdated(
+            _builder,
+            _commitment,
+            stake.stake,
+            stake.subscriptionEnd
+        );
     }
 
     /**
      * @notice Set minimal stake amount for builder
      * @param _minimalStake The amount of minimal stake
      */
-    function setMinimalStake(uint256 _minimalStake) public {
-        minimalStakes[msg.sender] = _minimalStake;
-        emit MinimalStakeUpdated(msg.sender, _minimalStake);
+    function updateBuilder(
+        uint256 _minimalStake,
+        uint256 _minimalSubscriptionPeriod
+    ) public {
+        BuilderInfo memory info;
+        info.minimalStake = _minimalStake;
+        info.minimalSubscriptionPeriod = _minimalSubscriptionPeriod;
+
+        builders[msg.sender] = info;
+
+        emit BuilderUpdated(
+            msg.sender,
+            _minimalStake,
+            _minimalSubscriptionPeriod
+        );
+    }
+
+    /**
+     * @notice Withdraw locked funds
+     */
+    function withdraw() public {
+        address _user = msg.sender;
+        require(timeLocks[_user].length > 0, "No locked funds");
+
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < timeLocks[_user].length; i++) {
+            uint256 elapsedTime = block.timestamp -
+                timeLocks[_user][i].startTime;
+            if (elapsedTime > timeLocks[_user][i].lockDuration) {
+                elapsedTime = timeLocks[_user][i].lockDuration;
+            }
+
+            uint256 releasableAmount = (elapsedTime *
+                timeLocks[_user][i].initialAmount) /
+                timeLocks[_user][i].lockDuration;
+            if (releasableAmount > timeLocks[_user][i].remainingAmount) {
+                releasableAmount = timeLocks[_user][i].remainingAmount;
+            }
+
+            if (releasableAmount > 0) {
+                timeLocks[_user][i].remainingAmount -= releasableAmount;
+                totalAmount += releasableAmount;
+            }
+
+            // if time lock is empty, remove it
+            if (timeLocks[_user][i].remainingAmount == 0) {
+                _removeTimeLock(_user, i);
+            }
+        }
+
+
+        require(totalAmount > 0, "Nothing to withdraw");
+        (bool sent, ) = _user.call{value: totalAmount}("");
+        require(sent, "Failed to release funds");
+        emit Withdrawal(_user, totalAmount);
+    }
+
+    /**
+     * @notice Calculate amount available to withdraw
+     * @return _withdrawableAmount The amount available to withdraw from time locks
+     */
+    function withdrawableAmount() public view returns (uint256) {
+        address _user = msg.sender;
+        require(timeLocks[_user].length > 0, "No locked funds");
+
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < timeLocks[_user].length; i++) {
+            uint256 elapsedTime = block.timestamp -
+                timeLocks[_user][i].startTime;
+            if (elapsedTime > timeLocks[_user][i].lockDuration) {
+                elapsedTime = timeLocks[_user][i].lockDuration;
+            }
+
+            uint256 releasableAmount = (elapsedTime *
+                timeLocks[_user][i].initialAmount) /
+                timeLocks[_user][i].lockDuration;
+            if (releasableAmount > timeLocks[_user][i].remainingAmount) {
+                releasableAmount = timeLocks[_user][i].remainingAmount;
+            }
+
+            if (releasableAmount > 0) {
+                totalAmount += releasableAmount;
+            }
+        }
+
+        return totalAmount;
     }
 
     /**
@@ -70,8 +191,95 @@ contract BuilderStaking is Ownable {
         address _builder,
         bytes32 _commitment
     ) public view returns (bool) {
+        uint256 minimalStake = builders[_builder].minimalStake;
+        return minimalStake > 0 && stakes[_commitment].stake >= minimalStake;
+    }
+
+    /**
+     * @notice Get time locks count for specific address
+     * @param _address The time locks receiver address
+     * @return _timeLocksCount Time locks count for specified receiver
+     */
+    function timeLocksCount(address _address) public view returns (uint256) {
+        return timeLocks[_address].length;
+    }
+
+    /**
+     * @notice Get subscription period in blocks by builder address
+     * @param _builder The builder address
+     * @return _subscriptionPeriod Subscription period in blocks
+     */
+    function getSubscriptionPeriod(
+        address _builder,
+        uint256 _stakeAmount
+    ) public view returns (uint256) {
+        BuilderInfo memory info = builders[_builder];
+        require(info.minimalStake > 0, "Builder minimal stake is not set");
+        require(
+            info.minimalSubscriptionPeriod > 0,
+            "Builder minimal subscription period is not set"
+        );
+
         return
-            minimalStakes[_builder] > 0 &&
-            stakes[_commitment] >= minimalStakes[_builder];
+            _getSubscriptionPeriod(
+                info.minimalStake,
+                info.minimalSubscriptionPeriod,
+                _stakeAmount
+            );
+    }
+
+    /**
+     * @notice Lock funds for block time period for specific address
+     * @param _address Lock address
+     * @param _amount Lock amount
+     * @param _lockDuration Lock duration in blocks
+     */
+    function _lockFunds(
+        address _address,
+        uint256 _amount,
+        uint256 _lockDuration
+    ) private {
+        require(_lockDuration > 0, "Lock duration should be positive");
+        require(_amount > 0, "Amount should be positive");
+
+        timeLocks[_address].push(
+            TimeLock({
+                initialAmount: _amount,
+                remainingAmount: _amount,
+                startTime: block.timestamp,
+                lockDuration: _lockDuration
+            })
+        );
+    }
+
+    /**
+     * @notice Remove time lock item by address and index
+     * @param _address Lock address
+     * @param _index Lock index
+     */
+    function _removeTimeLock(address _address, uint256 _index) private {
+        require(_index < timeLocks[_address].length);
+        timeLocks[_address][_index] = timeLocks[_address][
+            timeLocks[_address].length - 1
+        ];
+        timeLocks[_address].pop();
+    }
+
+    /**
+     * @notice Get subscription period in blocks by
+     * minimal stake, minimal subscription period and stake amount
+     * @param _minimalStake Minimal stake of the builder
+     * @param _minimalSubscriptionPeriod Minimal subscription period of the builder
+     * @param _stakeAmount Stake amount
+     * @return _subscriptionPeriod Subscription period in blocks
+     */
+    function _getSubscriptionPeriod(
+        uint256 _minimalStake,
+        uint256 _minimalSubscriptionPeriod,
+        uint256 _stakeAmount
+    ) private pure returns (uint256) {
+        uint256 periodPerStake = (_minimalSubscriptionPeriod * BASE_DIVISOR) /
+            _minimalStake;
+        return (periodPerStake * _stakeAmount) / BASE_DIVISOR;
     }
 }
--- test/BuilderStaking.ts
+++ test/BuilderStaking.ts
@@ -1,4 +1,4 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture, mine } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
@@ -20,49 +20,84 @@ describe("BuilderStaking", function () {
     it("Should deposit successfully", async function () {
       const { builderStaking, builder, searcher, searcher2, commitmentAccount } = await loadFixture(deployBuilderStaking);
 
+      const minimalStake = 100;
+      const minimalSubscriptionPeriod = 1000;
+      await builderStaking.connect(builder).updateBuilder(minimalStake, minimalSubscriptionPeriod);
+
+      const blockNumber = (await builderStaking.provider.getBlock('latest')).number;
+
       const commitment = getCommitment(commitmentAccount.address, builder.address);
-      await builderStaking.connect(builder).setMinimalStake(100);
       await expect(builderStaking.connect(searcher).deposit(builder.address, commitment, { value: 100 }))
         .to.emit(builderStaking, "StakeUpdated")
-        .withArgs(builder.address, commitment, 100);
-      await expect(builderStaking.connect(searcher2).deposit(builder.address, commitment, { value: 50 }))
+        .withArgs(builder.address, commitment, 100, blockNumber + minimalSubscriptionPeriod + 1);
+      await expect(builderStaking.connect(searcher2).deposit(builder.address, commitment, { value: 200 }))
         .to.emit(builderStaking, "StakeUpdated")
-        .withArgs(builder.address, commitment, 150);
+        .withArgs(builder.address, commitment, 300, blockNumber + minimalSubscriptionPeriod * 3 + 1);
 
-      expect(await builderStaking.stakes(commitment)).to.equal(150);
-      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(150);
+      expect((await builderStaking.stakes(commitment)).stake).to.equal(300);
+      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(300 * 0.8);
     });
   });
 
   describe("Withdrawals", function () {
     it("Should revert if there is no balance", async function () {
-      const { builderStaking, searcher } = await loadFixture(deployBuilderStaking);
+      const { builderStaking, builder, searcher, commitmentAccount } = await loadFixture(deployBuilderStaking);
 
-      await expect(builderStaking.connect(searcher).withdraw()).to.be.revertedWith(
+      await expect(builderStaking.connect(builder).withdraw()).to.be.revertedWith(
+        "No locked funds"
+      );
+
+      const minimalStake = 100;
+      const minimalSubscriptionPeriod = 1000;
+
+      const commitment = getCommitment(commitmentAccount.address, builder.address);
+      await builderStaking.connect(builder).updateBuilder(minimalStake, minimalSubscriptionPeriod);
+      await builderStaking.connect(searcher).deposit(builder.address, commitment, { value: 100 });
+
+      await expect(builderStaking.connect(builder).withdraw()).to.be.revertedWith(
         "Nothing to withdraw"
       );
     });
 
     it("Should withdraw successfully", async function () {
-      const { builderStaking, owner, builder, searcher, searcher2, commitmentAccount } = await loadFixture(deployBuilderStaking);
+      const { builderStaking, builder, searcher, searcher2, commitmentAccount } = await loadFixture(deployBuilderStaking);
+      const minimalStake = 50;
+      const minimalSubscriptionPeriod = 1000;
       const commitment = getCommitment(commitmentAccount.address, builder.address);
 
+      await builderStaking.connect(builder).updateBuilder(minimalStake, minimalSubscriptionPeriod);
+
       await builderStaking.connect(searcher).deposit(builder.address, commitment, { value: 100 });
-      expect(await builderStaking.balances(builder.address)).to.be.equal(80);
+      expect((await builderStaking.stakes(commitment)).stake).to.be.equal(100);
 
       await builderStaking.connect(searcher2).deposit(builder.address, commitment, { value: 50 })
-      expect(await builderStaking.balances(builder.address)).to.be.equal(120);
+      expect((await builderStaking.stakes(commitment)).stake).to.be.equal(150);
 
-      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(150);
+      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(150 * 0.8);
+
+      await expect(builderStaking.connect(builder).withdraw()).to.be.revertedWith(
+        "Nothing to withdraw"
+      );
+
+      expect(await builderStaking.connect(builder).timeLocksCount(builder.address)).to.be.equal(2);
+
+      await mine(minimalSubscriptionPeriod);
 
       await expect(builderStaking.connect(builder).withdraw())
-        .to.emit(builderStaking, "BalanceUpdated")
-        .withArgs(builder.address, 0);
+        .to.emit(builderStaking, "Withdrawal")
+        .withArgs(builder.address, 100 * 0.8);
 
-      expect(await builderStaking.balances(builder.address)).to.be.equal(0);
-      expect(await builderStaking.balances(owner.address)).to.be.equal(30);
+      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(50 * 0.8);
+      expect(await builderStaking.connect(builder).timeLocksCount(builder.address)).to.be.equal(1);
 
-      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(30);
+      await mine(minimalSubscriptionPeriod);
+
+      await expect(builderStaking.connect(builder).withdraw())
+        .to.emit(builderStaking, "Withdrawal")
+        .withArgs(builder.address, 50 * 0.8);
+      expect(await builderStaking.connect(builder).timeLocksCount(builder.address)).to.be.equal(0);
+
+      expect(await builderStaking.provider.getBalance(builderStaking.address)).to.be.equal(0);
     });
   });
 
@@ -70,29 +105,29 @@ describe("BuilderStaking", function () {
     it("Should set minimal stake", async function () {
       const { builderStaking, builder } = await loadFixture(deployBuilderStaking);
 
-      await expect(builderStaking.connect(builder).setMinimalStake(1000))
-        .to.emit(builderStaking, "MinimalStakeUpdated")
-        .withArgs(builder.address, 1000);
+      await expect(await builderStaking.connect(builder).updateBuilder(100, 1000))
+        .to.emit(builderStaking, "BuilderUpdated")
+        .withArgs(builder.address, 100, 1000);
 
-      await expect(builderStaking.connect(builder).setMinimalStake(2000))
-        .to.emit(builderStaking, "MinimalStakeUpdated")
-        .withArgs(builder.address, 2000);
+      await expect(await builderStaking.connect(builder).updateBuilder(200, 2000))
+        .to.emit(builderStaking, "BuilderUpdated")
+        .withArgs(builder.address, 200, 2000);
     });
 
     it("Should change has minimal stake state", async function () {
       const { builderStaking, builder, searcher, commitmentAccount } = await loadFixture(deployBuilderStaking);
       const commitment = getCommitment(commitmentAccount.address, builder.address);
 
-      await expect(builderStaking.connect(builder).setMinimalStake(1000))
-        .to.emit(builderStaking, "MinimalStakeUpdated")
-        .withArgs(builder.address, 1000);
+      await expect(await builderStaking.connect(builder).updateBuilder(1000, 1000))
+        .to.emit(builderStaking, "BuilderUpdated")
+        .withArgs(builder.address, 1000, 1000);
 
       await builderStaking.connect(searcher).deposit(builder.address, commitment, { value: 1000 });
       expect(await builderStaking.hasMinimalStake(builder.address, commitment)).to.be.equal(true);
 
-      await expect(builderStaking.connect(builder).setMinimalStake(2000))
-        .to.emit(builderStaking, "MinimalStakeUpdated")
-        .withArgs(builder.address, 2000);
+      await expect(await builderStaking.connect(builder).updateBuilder(2000, 2000))
+        .to.emit(builderStaking, "BuilderUpdated")
+        .withArgs(builder.address, 2000, 2000);
 
       expect(await builderStaking.hasMinimalStake(builder.address, commitment)).to.be.equal(false);
     });
